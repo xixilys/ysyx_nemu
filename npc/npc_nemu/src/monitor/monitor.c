5a457511a73d764b1181d992eee75eec2fed7be2
ddddddd <1660509539@qq.com>
2022-11-15 16:43:16 +0800

    modify npc

--- npc/npc_nemu/src/monitor/monitor.c
+++ npc/npc_nemu/src/monitor/monitor.c
@@ -35,7 +35,32 @@ static void welcome() {
 void sdb_set_batch_mode();
 
 static char *log_file = NULL;
-static char *elf_file = NULL;
+
+
+
+
+elf_list *elf_read_list = NULL;
+static void elf_get(char * data) {
+  if(elf_read_list == NULL) {
+    elf_read_list = (elf_list *)malloc(sizeof(elf_list));
+    elf_read_list -> has_use = 1;
+    elf_read_list -> elf_file = data;
+    elf_read_list -> next = NULL;
+  }else {
+    elf_list * p = elf_read_list;
+    while(p->next != NULL) {
+      p = p -> next;
+    }
+    p->next = (elf_list *)malloc(sizeof(elf_list));
+    p->next->has_use = 1;
+    p->next->elf_file  = data;
+
+    p->next->next = NULL;
+  }
+  
+}
+
+
 static char *diff_so_file = NULL;
 static char *img_file = NULL;
 static int difftest_port = 1234;
@@ -62,13 +87,7 @@ static long load_img() {
   return size;
 }
 
- ElfN_Ehdr elf_header = {};
- Elf64_Phdr * elf_program_header = NULL;
- Elf64_Shdr * elf_section_header = NULL;
- char  *elf_string_table = NULL;
- char  *elf_sec_name_table = NULL;
- size_t sec_entry_num = 0;
- Elf64_Sym  * elf_symbol_table = NULL;
+
 
 
 
@@ -98,14 +117,13 @@ void printf_ftrace_type(int type,int *kongge_num) {
 void ftrace_loop_print(ftrace_type * loop,int index) {
   int i = index;
   int kongge_num = 0;
-
+  elf_list * p = NULL;
   for(;i < MAX_FTRACE_LOOP_DEPTH;i++) {
     printf("    pc -> %08lx ",ftrace_loop[i].pc);
-    // printf_kongge(kongge_num);
     printf_ftrace_type(ftrace_loop[i].type,&kongge_num);
-    // printf("index is %d\n",ftrace_loop[i].symbol_table_index);
     if(ftrace_loop[i].type == func_call) {
-      printf("@%08lx  %s\n",ftrace_loop[i].target_pc,&elf_string_table[elf_symbol_table[ftrace_loop[i].symbol_table_index].st_name]); 
+      p = ftrace_loop[i].elf_ptr;
+      printf("@%08lx  %s\n",ftrace_loop[i].target_pc,&p->elf_string_table[p->elf_symbol_table[ftrace_loop[i].symbol_table_index].st_name]); 
     }else{
       printf("@%08lx \n",ftrace_loop[i].target_pc); 
     }
@@ -114,31 +132,29 @@ void ftrace_loop_print(ftrace_type * loop,int index) {
   for(i = 0;i < index - 1;i++) {
      printf("    pc -> %08lx ",ftrace_loop[i].pc);
     printf_ftrace_type(ftrace_loop[i].type,&kongge_num);
-    // printf("index is %d\n",ftrace_loop[i].symbol_table_index);
+  
     if(ftrace_loop[i].type == func_call) {
-      printf("@%08lx  %s\n",ftrace_loop[i].target_pc,&elf_string_table[elf_symbol_table[ftrace_loop[i].symbol_table_index].st_name]); 
+      p = ftrace_loop[i].elf_ptr;
+      printf("@%08lx  %s\n",ftrace_loop[i].target_pc,&p->elf_string_table[p->elf_symbol_table[ftrace_loop[i].symbol_table_index].st_name]); 
     }else{
       printf("@%08lx \n",ftrace_loop[i].target_pc); 
     }
-    
-    // printf("@%08lx   \n",ftrace_loop[i].target_pc); 
-   // printf("mem_addr -> %lx  mem_data -> %016lx mem_len -> %x \n",ftrace_loop[i].paddr,ftrace_loop[i].data,ftrace_loop[i].len); 
   }
      printf("    pc -> %08lx ",ftrace_loop[i].pc);
     printf_ftrace_type(ftrace_loop[i].type,&kongge_num);
-    // printf("index is %d\n",ftrace_loop[i].symbol_table_index);
+
     if(ftrace_loop[i].type == func_call) {
-      printf("@%08lx  %s\n",ftrace_loop[i].target_pc,&elf_string_table[elf_symbol_table[ftrace_loop[i].symbol_table_index].st_name]); 
+       p = ftrace_loop[i].elf_ptr;
+      printf("@%08lx  %s\n",ftrace_loop[i].target_pc,&p->elf_string_table[p->elf_symbol_table[ftrace_loop[i].symbol_table_index].st_name]); 
     }else{
       printf("@%08lx \n",ftrace_loop[i].target_pc); 
     }
-    
-    // printf("@%08lx   \n",ftrace_loop[i].target_pc); 
 }
 
-
+#ifdef ITRACE_COND
  void get_jal_type (size_t now_pc,size_t target_pc,uint8_t inst_type,uint8_t reg_source,uint32_t imm_value) {
     ftrace_type temp_ftrace = {};
+  
     if(inst_type == jal_or_jalr) {
         if(reg_source == 1 && imm_value == 0) {
           // printf("ret ! @%lx\n",target_pc);
@@ -149,84 +165,94 @@ void ftrace_loop_print(ftrace_type * loop,int index) {
           ftrace_loop_push(ftrace_loop,temp_ftrace , &ftrace_loop_index);
           return;
         }
-        for(int i = 0;i < sec_entry_num ; i++) {
-            if(elf_symbol_table[i].st_value  == target_pc && ELF64_ST_TYPE(elf_symbol_table[i].st_info) == STT_FUNC) {
-               // ftrace_type temp_ftrace = {};
-                temp_ftrace.pc = now_pc;
-                temp_ftrace.target_pc = target_pc;
-                temp_ftrace.type = func_call;
-                // printf("i = %d\n",i);
-                temp_ftrace.symbol_table_index = i;
-                ftrace_loop_push(ftrace_loop,temp_ftrace , &ftrace_loop_index);
-                // printf("it is func!!! and pc = %lx  name = %s \n",target_pc,&elf_string_table[elf_symbol_table[i].st_name]);
-                return;
+        // printf("jal pc is %lx\n",now_pc);
+        // printf("target  pc is %lx\n",target_pc);
+        // printf("sec entry num is %lx\n",sec_entry_num);
+        if(elf_read_list != NULL) {
+          elf_list *p = elf_read_list;
+          do{
+            for(int i = 0;i < p->sec_entry_num ; i++) {
+            // printf("target  pc is %lx\n",target_pc);
+              if(p->elf_symbol_table[i].st_value  == target_pc && ELF64_ST_TYPE(p->elf_symbol_table[i].st_info) == STT_FUNC) {
+                // ftrace_type temp_ftrace = {};
+                  temp_ftrace.elf_ptr = p;
+                  temp_ftrace.pc = now_pc;
+                  temp_ftrace.target_pc = target_pc;
+                  temp_ftrace.type = func_call;
+                  // printf("i = %d\n",i);
+                  temp_ftrace.symbol_table_index = i;
+                  ftrace_loop_push(ftrace_loop,temp_ftrace , &ftrace_loop_index);
+                  // printf("it is func!!! and pc = %lx  name = %s \n",target_pc,&elf_string_table[elf_symbol_table[i].st_name]);
+                  return;
+              }
             }
-            
+            p = p->next;
+          }while(p != NULL);
+          
         }
+        
     }
 }
-
+#endif
 static void load_elf() {
-  if (elf_file == NULL) {
+  if (elf_read_list == NULL) {
     printf("No elf is given. ftrace is not working.");
     return ;
 
   }
+  elf_list * p = elf_read_list;
+  do{
+    char * elf_file = p -> elf_file;
 
     FILE *fp = fopen(elf_file, "rb");
-
     Assert(fp, "Can not open '%s'", elf_file);
+    int answer = fread(&p->elf_header,sizeof(p->elf_header),1,fp); 
 
-    int answer = fread(&elf_header,sizeof(elf_header),1,fp); 
+    p->elf_program_header = (Elf64_Phdr*)malloc(p->elf_header.e_phnum * p->elf_header.e_phentsize);
+    p->elf_section_header = (Elf64_Shdr*)malloc(p->elf_header.e_shnum * p->elf_header.e_shentsize);
 
-    elf_program_header = ( Elf64_Phdr * )malloc(elf_header.e_phnum * elf_header.e_phentsize);
-    elf_section_header = (Elf64_Shdr *) malloc(elf_header.e_shnum * elf_header.e_shentsize);
+    answer = fseek(fp,p->elf_header.e_phoff,SEEK_SET);
 
-    answer = fseek(fp,elf_header.e_phoff,SEEK_SET);
-
-    answer = fread(elf_program_header,elf_header.e_phnum * elf_header.e_phentsize,1,fp); 
+    answer = fread(p->elf_program_header,p->elf_header.e_phnum * p->elf_header.e_phentsize,1,fp); 
 
     printf("answer = %d\n",answer);
 
 
-    answer = fseek(fp,elf_header.e_shoff,SEEK_SET);
-    answer = fread(elf_section_header,elf_header.e_shnum * elf_header.e_shentsize,1,fp); 
+    answer = fseek(fp,p->elf_header.e_shoff,SEEK_SET);
+    answer = fread(p->elf_section_header,p->elf_header.e_shnum * p->elf_header.e_shentsize,1,fp); 
 
-    // int symble_table_index = 0;
+    for(int i = 0;i < p->elf_header.e_shnum;i++) {
+      if(p->elf_section_header[i].sh_type == SHT_STRTAB) {
 
-
-    for(int i = 0;i < elf_header.e_shnum;i++) {
-     // printf(" i = %d and sh_type = %d\n",i,elf_section_header[i].sh_type);
-      if(elf_section_header[i].sh_type == SHT_STRTAB) {
-
-        answer = fseek(fp,elf_section_header[i].sh_offset,SEEK_SET);
+        answer = fseek(fp,p->elf_section_header[i].sh_offset,SEEK_SET);
       
-        if(i != elf_header.e_shstrndx) {
-          elf_string_table = ( char  *)malloc(sizeof(char) * elf_section_header[i].sh_size);
-          answer = fread(elf_string_table,sizeof(char) * elf_section_header[i].sh_size ,1,fp); 
+        if(i != p->elf_header.e_shstrndx) {
+          p->elf_string_table = (char*)malloc(sizeof(char) * p->elf_section_header[i].sh_size);
+          answer = fread(p->elf_string_table,sizeof(char) * p->elf_section_header[i].sh_size ,1,fp); 
          
-        }else if(i == elf_header.e_shstrndx) {
-          elf_sec_name_table =  (char  *)malloc(sizeof(char) * elf_section_header[i].sh_size);
-          answer = fread(elf_sec_name_table,sizeof(char) * elf_section_header[i].sh_size ,1,fp); 
+        }else if(i == p->elf_header.e_shstrndx) {
+          p->elf_sec_name_table = (char*)malloc(sizeof(char) * p->elf_section_header[i].sh_size);
+          answer = fread(p->elf_sec_name_table,sizeof(char) * p->elf_section_header[i].sh_size ,1,fp); 
         }
           
-      }else if(elf_section_header[i].sh_type == SHT_SYMTAB) { 
-        // symble_table_index = i;
-        // printf("index = %d\n",symble_table_index);
-        sec_entry_num =  elf_section_header[i].sh_entsize;
-        elf_symbol_table =  (Elf64_Sym  * )malloc(sizeof(Elf64_Sym)* elf_section_header[i].sh_entsize);
+      }else if(p->elf_section_header[i].sh_type == SHT_SYMTAB) { 
+        
+        p->sec_entry_num = p->elf_section_header[i].sh_size /  p->elf_section_header[i].sh_entsize;
+    
+        p->elf_symbol_table = (Elf64_Sym*)malloc(p->elf_section_header[i].sh_size );
 
-        answer = fseek(fp,elf_section_header[i].sh_offset,SEEK_SET);
-        answer = fread(elf_symbol_table,sizeof(Elf64_Sym) * elf_section_header[i].sh_entsize,1,fp); 
+        answer = fseek(fp,p->elf_section_header[i].sh_offset,SEEK_SET);
+        answer = fread(p->elf_symbol_table,p->elf_section_header[i].sh_size,1,fp); 
        
       }
     }
   
-  //  for(int i = 0; i < elf_section_header[sec_entry_num].sh_entsize; i++) {
-  //     printf("index = %02d value = %08lx type -> %x and %s\n",i,elf_symbol_table[i].st_value,ELF64_ST_TYPE(elf_symbol_table[i].st_info),&elf_string_table[elf_symbol_table[i].st_name]);
-  //  }
+
   
   fclose(fp);
+  p = p->next;
+
+  }while( p != NULL);
 
 }
 static int parse_args(int argc, char *argv[]) {
@@ -248,7 +274,7 @@ static int parse_args(int argc, char *argv[]) {
       case 'p': sscanf(optarg, "%d", &difftest_port); break;
       case 'l': log_file     = optarg; break;
       case 'd': diff_so_file = optarg; break;
-      case 'f': elf_file     = optarg;  break;
+      case 'f': elf_get(optarg);  break;
       case 1:   img_file     = optarg; return optind - 1;
       default:
         printf("Usage: %s [OPTION...] IMAGE [args]\n\n", argv[0]);
