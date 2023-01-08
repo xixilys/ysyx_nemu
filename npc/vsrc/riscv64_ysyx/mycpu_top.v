a9bd1d12a28cf323071a27dbe8e6d2ca29b6243a
ddddddd <1660509539@qq.com>
2023-01-08 14:51:10 +0800

    fix some git ignore

--- npc/vsrc/riscv64_ysyx/mycpu_top.v
+++ npc/vsrc/riscv64_ysyx/mycpu_top.v
@@ -1,6 +1,14 @@
 /* verilator lint_off WIDTH */ 
 /* verilator lint_off WIDTH */ 
 /* verilator lint_off WIDTH */ 
+/* verilator lint_off WIDTH */ 
+/* verilator lint_off WIDTH */ 
+/* verilator lint_off WIDTH */ 
+/* verilator lint_off WIDTH */ 
+/* verilator lint_off WIDTH */ 
+/* verilator lint_off WIDTH */ 
+/* verilator lint_off WIDTH */ 
+/* verilator lint_off WIDTH */ 
 module alu(
   input  [23:0] io_ctrl,
   input  [63:0] io_in1,
