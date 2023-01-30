a347ed6fc15e37d2fb754bf94fefc8d915eac5c6
ddddddd <1660509539@qq.com>
2023-01-30 12:33:09 +0800

    add some setup env

--- ysyx-exam/setup-env.sh
+++ ysyx-exam/setup-env.sh
@@ -2,3 +2,4 @@ export NEMU_HOME=`readlink -e nemu`
 export AM_HOME=`readlink -e abstract-machine`
 export NAVY_HOME=`readlink -e navy-apps`
 export NPC_HOME=`readlink -e npc`
+export NPC_NEMU_HOME=$NPC_HOME/npc_nemu
